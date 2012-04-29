e9925b56613d36de8101942bd31aa63251964a5d
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-04-29 12:07:40 +1000

    index.html and BC API status

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,42 @@
 import os
 
 import cherrypy
+from cherrypy.lib.static import serve_file
+import requests
+from requests.auth import HTTPBasicAuth
+
+class BigCommerceAPI(object):
+    """ ad-hoc BigCommerce API wrapper """
+
+    user = 'admin'
+    token = 'dd0f6a14c5dd516cde2e5cbf64342347f24a9cc8'
+    url = 'https://store-bd2f8.mybigcommerce.com/api/v2/'
+
+    def authentication(self):
+        return HTTPBasicAuth(self.user, self.token)
+
+    def get(self, api):
+        url = "%s%s.json" % (self.url, api)
+        return requests.get(url, auth=self.authentication())
+
 
 class App:
 
     @cherrypy.expose
     def index(self):
-        return "OK"
+        """ single HTML page web application """
+        single = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'index.html')
+        return serve_file(single)
+
+    @cherrypy.expose
+    def statuses(self):
+        """ contact BigCommerce to get list of all statuses """
+        big = BigCommerceAPI()
+        response = big.get('orderstatuses')
+        return response.text
+
 
 application = cherrypy.tree.mount(App(), '')
 
